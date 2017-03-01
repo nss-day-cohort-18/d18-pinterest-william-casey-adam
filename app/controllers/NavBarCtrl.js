914dac529e06d3042d68b4ac488e2fed218d67a9
Adam Myers <Tavern@adams-mac.attlocal.net>
2017-02-28 20:49:58 -0600

    added a jumbotron to encourage login

--- app/controllers/NavBarCtrl.js
+++ app/controllers/NavBarCtrl.js
@@ -1,6 +1,9 @@
 "use strict";
 
-app.controller("NavBarCtrl", function() {
-	console.log("Loaded a blank NavBarCtrl.js :D");
-	console.log("The error for 404 (Not Found) is the for the image source in the list.html, do not be alarmed");
+app.controller("NavBarCtrl", function($scope, AuthorizeFactory) {
+	console.log("Loaded NavBarCtrl.js :D");
+
+	$scope.login = function() {
+		AuthorizeFactory.authWithProvider();
+	};
 });
