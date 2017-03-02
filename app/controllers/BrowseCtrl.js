b4010af0f674c1c45ef658842f8e2081c2523e6c
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-02 11:57:22 -0600

    added placeholder modal

--- app/controllers/BrowseCtrl.js
+++ app/controllers/BrowseCtrl.js
@@ -1,17 +1,14 @@
 "use strict";
 
 app.controller("BrowseCtrl", function($scope, $location, PinsFactory) {
-
-console.log("Loaded a blank BrowseCtrl.js :D");
-
-
-		PinsFactory.browsePins()
-			.then(function(returnedPins){
-				console.log('returnedPins:', returnedPins);
-				$scope.pins = returnedPins.data;
-				console.log('$scope.pins:', $scope.pins);
-			});
-
-
-
+	PinsFactory.browsePins()
+	.then(function(returnedPins){
+		console.log('returnedPins:', returnedPins);
+		$scope.pins = returnedPins.data;
+		console.log('$scope.pins:', $scope.pins);
+	});
+    $scope.addPin = function() {
+        console.log("Saving pin: ", $scope.pin);
+        $scope.pin = {};
+    };
 });
