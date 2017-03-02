245af85446f904abb2e8f102f6d70ca3390d6d9e
Adam Myers <Tavern@adams-mac.attlocal.net>
2017-03-01 23:29:11 -0600

    work in progress of properly functioning modals

--- app/controllers/NavBarCtrl.js
+++ app/controllers/NavBarCtrl.js
@@ -1,10 +1,38 @@
 "use strict";
 
-app.controller("NavBarCtrl", function($scope, AuthorizeFactory) {
-	console.log("Loaded NavBarCtrl.js :D");
+app.controller("NavBarCtrl", function($scope, $window, $location, AuthorizeFactory) {
+
+	$scope.isLoggedIn = false;
+	$scope.uid = null;
+
+	firebase.auth().onAuthStateChanged( function(user){
+		if (user) {
+			$scope.isLoggedIn = true;
+			$scope.uid = user.uid;
+			console.log("uid: ", $scope.uid, "isLoggedIn: ", $scope.isLoggedIn);
+		} else {
+			$scope.isLoggedIn = false;
+			$scope.uid = null;
+			console.log("logged in: ", $scope.isLoggedIn);
+			$window.location.href = "#!/login";
+		}
+	});
 
 	$scope.login = function() {
 		console.log("login function");
 		AuthorizeFactory.authWithProvider();
 	};
+
+	$scope.logout = function() {
+		console.log("logout function");
+		AuthorizeFactory.logoutUser()
+		.then(function() {
+			$scope.$apply();
+		});
+	};
+
+	$scope.Profile = function() {
+		$window.location.url = "#!/profile/{{$scope.uid}}";
+	};
+
 });
---
+++ app/factories/SearchFilter.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.factory("SearchFilter", function() {
+	return {
+		search: ""
+	};
+});
