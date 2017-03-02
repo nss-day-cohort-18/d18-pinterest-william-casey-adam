6c8384698ebfaacf1be1ef781551553255ee6f38
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-03-02 13:32:06 -0600

    modal returns an object

--- app/controllers/BrowseCtrl.js
+++ app/controllers/BrowseCtrl.js
@@ -1,13 +1,22 @@
 "use strict";
 
-app.controller("BrowseCtrl", function($scope, $location, PinsFactory) {
+app.controller("BrowseCtrl", function($scope, $location, PinsFactory, AuthorizeFactory) {
+    $scope.pin = {};
 	PinsFactory.browsePins()
 	.then(function(returnedPins){
 		console.log('returnedPins:', returnedPins);
 		$scope.pins = returnedPins.data;
 		console.log('$scope.pins:', $scope.pins);
 	});
+    $scope.getPinInfo = function (thisPin) {
+        console.log('thisPin =', thisPin);
+        $scope.pin = thisPin;
+    };
+
     $scope.addPin = function() {
+        let currentUser = AuthorizeFactory.getUser();
+        console.log('currentUser in addPin = ', currentUser);
+        $scope.pin.useruid = currentUser;
         console.log("Saving pin: ", $scope.pin);
         $scope.pin = {};
     };
